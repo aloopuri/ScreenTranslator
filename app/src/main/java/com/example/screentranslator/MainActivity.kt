e63d3ee15de17773ea0b355b14d8cd2a508a3d33
aloopuri <thassan577@gmail.com>
2023-01-06 18:23:30 +0000

    Add title

--- app/src/main/java/com/example/screentranslator/MainActivity.kt
+++ app/src/main/java/com/example/screentranslator/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.screentranslator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.screentranslator.ui.theme.ScreenTranslatorTheme
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ScreenTranslateHomeScreen()
                 }
             }
         }
@@ -30,14 +35,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ScreenTranslateHomeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            fontSize = 24.sp,
+            color = Color(0xFF3ddc84),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(Modifier.height(16.dp))
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ScreenTranslatorTheme {
-        Greeting("Android")
+        ScreenTranslateHomeScreen()
     }
 }
